23d418f04c23359a1ef7ae1573becb91657d5ee1
Olga <olyayoga74@gmail.com>
2024-10-30 12:48:20 +0100

    parameter hinzuzugefügt

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function text() {
   document.body.innerHTML = "Hello World";
 }
-text();
+//text();
 
 let arr = ["Mohab", "Olga", "Guest"];
-let div = document.getElementById("div");
+let div = document.getElementById("div1");
 let bM = document.getElementById("M");
 let bO = document.getElementById("O");
 function helloM(arr) {
