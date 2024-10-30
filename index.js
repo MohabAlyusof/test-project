9d1b65bc803dc90ea8939ebe3b7ea6819a561f22
Olga <olyayoga74@gmail.com>
2024-10-30 11:55:13 +0100

    button click function

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function text() {
   document.body.innerHTML = "Hello World";
 }
 text();
+
+let arr = ["Mohab", "Olga", "Guest"];
+let div = document.getElementById("div");
+let bM = document.getElementById("M");
+let bO = document.getElementById("O");
+function helloM(arr) {
+  div.innerHTML = arr[0];
+}
+function helloO(arr) {
+  div.innerHTML = arr[1];
+}
