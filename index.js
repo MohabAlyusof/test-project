192bfb2c78bf5e9b4429347a4e13b96f5b1d1741
Mohab Alyusof <Mohabalusef-1997@hotmail.com>
2024-10-30 12:50:32 +0100

    mohab

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ function text() {
   document.body.innerHTML = "Hello World";
 }
 
-text();
-
 let arr = ["Mohab", "Olga", "Guest"];
-let div = document.getElementById("div");
+let div = document.getElementById("div1");
 let bM = document.getElementById("M");
 let bO = document.getElementById("O");
 function helloM(arr) {
